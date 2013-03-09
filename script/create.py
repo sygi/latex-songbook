ebb16c7e25043d3062ef73745dfccad577b15c81
Jakub Sygnowski <sygnowski@gmail.com>
2013-03-09 17:58:49 +0100

    skrypt generujacy slownik i ToC

--- script/create.py
+++ script/create.py
@@ -1,1 +1,30 @@
 #/usr/bin/python2
+
+import os
+
+files = os.listdir('songs')
+
+out = open('spiewnik.tex', 'w')
+
+out.write("""\\documentclass{book}
+\\usepackage[chordbk]{songbook}
+\\usepackage[T1]{polski}
+\\usepackage[utf8]{inputenc}
+
+\\makeTitleContents
+\\begin{document}
+
+""")
+
+for f in files:
+    g = open('songs/' + f, 'r')
+    out.write(g.read())
+
+out.write("""\\end{document}
+""")
+
+out.close()
+
+os.system('pdflatex spiewnik.tex')
+os.system('pdflatex toc.tex\n')
+
