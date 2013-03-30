e57cc5c59eab3ebf64718bf27dea7559d4fe725c
Jakub Sygnowski <sygnowski@gmail.com>
2013-03-30 12:44:33 +0100

    linki w toc

--- script/create.py
+++ script/create.py
@@ -4,12 +4,16 @@ import os
 
 files = os.listdir('songs')
 
+files.sort()
+#tutaj mozna zrobic odpowiednie sortowanie files
+
 out = open('spiewnik.tex', 'w')
 
 out.write("""\\documentclass{book}
 \\usepackage[chordbk]{songbook}
 \\usepackage[T1]{polski}
 \\usepackage[utf8]{inputenc}
+\\usepackage{hyperref}
 
 \\makeTitleContents
 \\begin{document}
